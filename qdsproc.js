dcaedf2ba26ac91d15a2828f7076000b81a54892
Martin Holters <martin.holters@hsu-hh.de>
2018-10-08 09:20:20 +0200

    Support more than one channel

--- qdsproc.js
+++ qdsproc.js
@@ -4,15 +4,27 @@ class QDSProcessor extends AudioWorkletProcessor {
 
     this.qt = 0.25;
     this.withDither = false;
-    this.ditherstate = 0.0;
+    this.ditherstate = new Float32Array(0);
     this.dithergen = this.rectDither;
     this.withNoiseShaping = true;
     this.h = Float32Array.from([1]);
-    this.nsState = new Float32Array(1);
+    this.nsState = new Array(0);
 
     var _this = this;
     this.port.onmessage = function(event) {
       _this[event.data.param] = event.data.value;
+    }
+  }
+
+  setChannelCount(nc) {
+    if (this.ditherstate.length != nc) {
+      this.ditherstate = new Float32Array(nc);
+    }
+    if (this.nsState.length != nc) {
+      this.nsState = new Array(nc);
+      for (var i = 0; i < this.nsState.length; i++) {
+        this.nsState[i] = new Float32Array(this.h.length);
+      }
     }
   }
 
@@ -45,44 +57,48 @@ class QDSProcessor extends AudioWorkletProcessor {
     } else if (order==9) {
       this.h = Float32Array.from([2.412, -3.370, 3.937, -4.174, 3.353, -2.205, 1.281, -0.569, 0.0847]);
     }
-    this.nsState = new Float32Array(this.h.length);
+    for (var i = 0; i < this.nsState.length; i++) {
+      this.nsState[i] = new Float32Array(this.h.length);
+    }
   }
 
-  rectDither() {
+  rectDither(channel) {
     return Math.random() - 0.5;
   }
-  triDither() {
+  triDither(channel) {
     return Math.random() + Math.random() - 1.0;
   }
-  hpDither() {
+  hpDither(channel) {
     var rnd = Math.random() - 0.5;
-    var d = rnd - this.ditherstate;
-    this.ditherstate = rnd;
+    var d = rnd - this.ditherstate[channel];
+    this.ditherstate[channel] = rnd;
     return d;
   }
 
   process(inputs, outputs, parameters) {
-    // TODO deal with more than one channel
-    var inputData = inputs[0][0];
-    var outputData = outputs[0][0];
-    for (var sample = 0; sample < inputData.length; sample++) {
-      var input = inputData[sample];
-      if (this.withNoiseShaping) {
-        for (var i = 0; i < this.h.length; i++) {
-          input -= this.h[i] * this.nsState[i];
+    this.setChannelCount(inputs[0].length)
+    for (var channel = 0; channel < inputs[0].length; channel++) {
+      var inputData = inputs[0][channel];
+      var outputData = outputs[0][channel];
+      for (var sample = 0; sample < inputData.length; sample++) {
+        var input = inputData[sample];
+        if (this.withNoiseShaping) {
+          for (var i = 0; i < this.h.length; i++) {
+            input -= this.h[i] * this.nsState[channel][i];
+          }
         }
+        var d_rand = 0.0;
+        if (this.withDither) {
+          d_rand = this.dithergen(channel);
+        }
+        var xr = input;
+        var tmpOutput = this.qt * Math.round(xr/this.qt + d_rand);
+        for (var i = this.h.length-1; i >= 0; i--) {
+          this.nsState[channel][i] = this.nsState[channel][i-1];
+        }
+        this.nsState[channel][0] = tmpOutput - input;
+        outputData[sample] = tmpOutput;
       }
-      var d_rand = 0.0;
-      if (this.withDither) {
-        d_rand = this.dithergen();
-      }
-      var xr = input;
-      var tmpOutput = this.qt * Math.round(xr/this.qt + d_rand);
-      for (var i = this.h.length-1; i >= 0; i--) {
-        this.nsState[i] = this.nsState[i-1];
-      }
-      this.nsState[0] = tmpOutput - input;
-      outputData[sample] = tmpOutput;
     }
 
     return true;
