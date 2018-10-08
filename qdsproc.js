aadec86f20975655a09344f5feacb1b6267f80e0
Martin Holters <martin.holters@hsu-hh.de>
2018-10-08 11:16:28 +0200

    Generalize setupAudio

--- qdsproc.js
+++ qdsproc.js
@@ -12,7 +12,11 @@ class QDSProcessor extends AudioWorkletProcessor {
 
     var _this = this;
     this.port.onmessage = function(event) {
-      _this[event.data.param] = event.data.value;
+      if (event.data.action == 'set-property') {
+        _this[event.data.param] = event.data.value;
+      } else if (event.data.action == 'list-properties') {
+        _this.port.postMessage({response: 'list-properties', properties: ['w', "dither", 'dithertype', 'noiseshaping', 'noiseshapingfilter']});
+      }
     }
   }
 
