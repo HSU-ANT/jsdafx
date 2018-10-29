1c2560f086d351f03b010a8cb4e943a83907e068
Martin Holters <martin.holters@hsu-hh.de>
2018-10-29 14:01:20 +0100

    Move signal drawing code to new class SignalGraph

--- qds.js
+++ qds.js
@@ -1,9 +1,6 @@
-import { FunctionGraph, setupAudio, setupPlayerControls } from './common.js';
+import { SignalGraph, setupAudio, setupPlayerControls } from './common.js';
 
 window.addEventListener('load', async () => {
-  const graph = new FunctionGraph(document.getElementById('funccanvas'));
-  graph.ylim = [-130, 0];
-
   const audioProc = await setupAudio('qdsproc.js', 'qds-processor');
   audioProc.proc.w = 16;
   audioProc.proc.dither = true;
@@ -16,60 +13,24 @@ window.addEventListener('load', async () => {
     { type: 'remote', label: 'Music', url: 'audio/unfinite_function.mp3' },
   ]);
 
-  const frequencies = new Float32Array(audioProc.getFrequencyDomainData());
-  for (let i = 0; i < frequencies.length; i++) {
-    frequencies[i] = i * 22050 / (frequencies.length-1);
-  }
-  const timeindices = new Float32Array(audioProc.getTimeDomainData());
-  for (let i = 0; i < timeindices.length; i++) {
-    timeindices[i] = i;
-  }
-  let drawWave = false;
-  function drawSignal() {
-    setTimeout(() => requestAnimationFrame(drawSignal), 40);
-    if (drawWave) {
-      const data = audioProc.getTimeDomainData();
-      graph.drawData(timeindices, data);
-    } else {
-      const data = audioProc.getFrequencyDomainData();
-      graph.drawData(frequencies, data);
-    }
-  }
-  drawSignal();
+  const graph = new SignalGraph(audioProc, document.getElementById('funccanvas'));
   const cblinear = document.getElementById('linear');
   cblinear.checked = false;
   cblinear.onchange = function (event) {
-    if (!drawWave) {
-      graph.logx = !event.target.checked;
-    }
+    graph.freqLinear = event.target.checked;
   };
-  function setDrawWave(b) {
-    drawWave = b;
-    if (b) {
-      graph.logx = false;
-      graph.ylim = [-1, 1];
-      graph.xlim= [0, timeindices.length-1];
-      graph.xlabel = 'time in samples';
-      graph.ylabel = 'amplitude';
-      cblinear.style.visibility = 'hidden';
-      cblinear.labels[0].style.visibility = 'hidden';
-    } else {
-      graph.xlim = [50, 20000];
-      graph.logx = !cblinear.checked;
-      graph.ylim = [-130, 0];
-      graph.xlabel = 'frequency in Hz';
-      graph.ylabel = 'magnitude in dB';
-      cblinear.style.visibility = 'visible';
-      cblinear.labels[0].style.visibility = 'visible';
-    }
-  }
+  const setDrawWave = (b) => {
+    graph.drawWave = b;
+    cblinear.style.visibility = cblinear.labels[0].style.visibility =
+      b ? 'hidden' : 'visible';
+  };
   document.getElementById('spectrum').onchange = (event) => {
     setDrawWave(!event.target.checked);
   };
   document.getElementById('waveform').onchange = (event) => {
     setDrawWave(event.target.checked);
   };
-  setDrawWave(false);
+
   document.getElementById('wordlength').value = 16;
   document.getElementById('wordlength').onchange = function (event) {
     audioProc.proc.w = event.target.value;
