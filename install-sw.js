a208951519b999ce0341b944f3beaa5a1eee7804
Martin Holters <martin.holters@hsu-hh.de>
2018-11-13 08:54:38 +0100

    Update to bdeffd186511843e0fa60f0aa330abdf0f847527

--- install-sw.js
+++ install-sw.js
@@ -1,1 +1,1 @@
-"serviceWorker"in navigator&&window.addEventListener("load",async()=>{try{await navigator.serviceWorker.register("sw.js")}catch(r){console.warn("ServiceWorker registration failed: ",r)}});export{};
+"serviceWorker"in navigator&&window.addEventListener("load",async()=>{try{await navigator.serviceWorker.register("sw.js",{type:"module"})}catch(e){console.warn("ServiceWorker registration failed: ",e)}});export{};
